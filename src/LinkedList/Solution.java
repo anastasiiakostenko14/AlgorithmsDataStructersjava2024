cef543559d16c3b7ce0fcd795f00fc36a9da5f11
anastasiiakostenko14 <anastasiia.kostenko@stud.onu.edu.ua>
2024-10-07 21:08:29 +0300

    10-LinkedList

--- src/LinkedList/Solution.java
+++ src/LinkedList/Solution.java
@@ -6,12 +6,12 @@ class Solution {
         ListNode current = temp;
 
         while(list1!=null && list2!=null){
-            if(list1.val < list2.val){
-                current.next = list1;
-                list1 = list1.next;
-            } else{
+            if(list1.val > list2.val){
                 current.next = list2;
                 list2 = list2.next;
+            } else{
+                current.next = list1;
+                list1 = list1.next;
             }
             current = current.next;
         }
